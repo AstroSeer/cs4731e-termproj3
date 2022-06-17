6dd7abaa25f7e3566a2007c7543b2ae02693a42c
AstroSeer <matthewnagy@verizon.net>
2022-06-17 18:23:24 -0400

    starter code

---
+++ FinalProjectStarter/main.js
@@ -0,0 +1,33 @@
+// WebGL globals
+let canvas;
+let gl;
+let program;
+
+
+/**
+ * Sets up WebGL and enables the features this program requires.
+ */
+function main() {
+    // Retrieve <canvas> element
+    canvas = document.getElementById('webgl');
+
+    // Get the rendering context for WebGL
+    gl = WebGLUtils.setupWebGL(canvas);
+
+    //Check that the return value is not null.
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    // Set viewport
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    // Set clear color
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Initialize shaders
+    program = initShaders(gl, "vshader", "fshader");
+    gl.useProgram(program);
+
+}
