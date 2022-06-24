4cb6f7de6e16f9f7c7fcfd06ba050e3bc531a7f3
AstroSeer <matthewnagy@verizon.net>
2022-06-24 13:44:59 -0400

    added code for setting up rendering

--- FinalProjectStarter/main.js
+++ FinalProjectStarter/main.js
@@ -3,6 +3,20 @@ let canvas;
 let gl;
 let program;
 
+var modelViewMatrixLoc;
+var projectionMatrixLoc;
+var modelViewMatrix;
+var projectionMatrix;
+
+var fov = 30;
+var aspect = 1;
+var near = 0.1;
+var far = 100;
+
+var eye;
+var at = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, -1.0, 0.0);
+
 
 /**
  * Sets up WebGL and enables the features this program requires.
@@ -24,14 +38,14 @@ function main() {
     gl.viewport(0, 0, canvas.width, canvas.height);
 
     // Set clear color
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     // Initialize shaders
     program = initShaders(gl, "vshader", "fshader");
     gl.useProgram(program);
 
-    loadFile("https://web.cs.wpi.edu/~jmcuneo/cs4731/project3_1/bunny.obj", "OBJ");
-    
+    loadFile("https://web.cs.wpi.edu/~jmcuneo/cs4731/project3_1/car.obj", "OBJ");
+
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
@@ -40,9 +54,22 @@ function main() {
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
     
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+
     render();
+
 }
 
 function render() {
-	gl.drawArrays(gl.LINES, 0, vertices.length);
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    eye = vec3(0, 0, -1);
+    modelViewMatrix = lookAt(eye, at , up);
+    projectionMatrix = perspective(fov, aspect, near, far);
+
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
+    
+	gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length);
+    requestAnimFrame(render);
 }
