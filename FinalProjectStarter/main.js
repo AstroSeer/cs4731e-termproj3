71e08978263c26e71bca3a1ba865c5f0a045cfb0
AstroSeer <matthewnagy@verizon.net>
2022-06-23 22:28:03 -0400

    i literally dont know

--- FinalProjectStarter/main.js
+++ FinalProjectStarter/main.js
@@ -24,12 +24,25 @@ function main() {
     gl.viewport(0, 0, canvas.width, canvas.height);
 
     // Set clear color
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
     // Initialize shaders
     program = initShaders(gl, "vshader", "fshader");
     gl.useProgram(program);
 
-    var aaa = loadFile("https://web.cs.wpi.edu/~jmcuneo/cs4731/project3_1/", "OBJ");
-    console.log(aaa);
+    loadFile("https://web.cs.wpi.edu/~jmcuneo/cs4731/project3_1/bunny.obj", "OBJ");
+    
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation( program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+    
+    render();
+}
+
+function render() {
+	gl.drawArrays(gl.LINES, 0, vertices.length);
 }
